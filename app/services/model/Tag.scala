730b54a3a0442929289053fcd355c366ec6352a6
Harsh Keswani <hkeswani@umass.edu>
2017-10-11 15:03:28 -0700

    Tags routes

--- app/services/model/Tag.scala
+++ app/services/model/Tag.scala
@@ -2,9 +2,11 @@ package services.model
 
 import java.util.UUID
 
+import org.joda.time.DateTime
+
 /**
  * Created by v723840 on 10/11/17.
  */
-class Tag (
-          id: UUID = UUID.randomUUID(),
-          name: String)
+case class Tag (id: UUID = UUID.randomUUID(),
+          name: String,
+      	  createdAt: DateTime = new DateTime)
